d83f4e28617c0aa8c937bfc8fed297aff2e48177
Leonardo Pires Goss <goss.leonardo3@gmail.com>
2025-06-09 12:09:18 -0300

    feat: add user auth using devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   enum role: { customer: 0, admin: 1 }
 
+  has_many :user_books, dependent: :destroy
   has_many :books, through: :user_books
 
   validates :name, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'home#index'
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

86c78a2bb40fc8d2c7c886b45ea557461b90d2b6
Leonardo Pires Goss <goss.leonardo3@gmail.com>
2025-06-12 16:59:06 -0300

    feat!: add tailwindcss

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
+  
+  resources :books
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
